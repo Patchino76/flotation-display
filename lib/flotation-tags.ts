1f696e408e7332064cd31d0f456ed3ca3423cb92
Svetoslav Lyubenov <patchino76@gmail.com>
2025-12-17 09:53:42 +0200

    tags updated

--- lib/flotation-tags.ts
+++ lib/flotation-tags.ts
@@ -1,20 +1,20 @@
-export const _FLOTATION_TAGS_OLD = {
-  extraction: 1790,
-  p1: 1430,
-  p4: 1433,
-  pP: 1435,
-  p2: 1431,
-  p5: 1434,
-  pK: 1438,
-  p3: 1432,
-  cd: 1436,
-  pO: 1600,
-  p0: 61,
-} as const;
+// export const _FLOTATION_TAGS_OLD = {
+//   extraction: 1790,
+//   p1: 1430,
+//   p4: 1433,
+//   pP: 1435,
+//   p2: 1431,
+//   p5: 1434,
+//   pK: 1438,
+//   p3: 1432,
+//   cd: 1436,
+//   pO: 1600,
+//   p0: 61,
+// } as const;
 
 export const FLOTATION_TAGS = {
   extraction: 1790,
-  p0: 61,
+  p0: 5586,
   p1: 53,
   p2: 54,
   p3: 55,
